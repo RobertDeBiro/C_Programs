152c3fc111b8a7ee1a59fd2cee0abb0b3f94b9d8
RobertDeBiro <robert.maric.92@gmail.com>
2023-02-27 13:12:14 +0100

    Adding wait()

--- Processes/2.wait.c
+++ Processes/2.wait.c
@@ -1,4 +1,15 @@
-// Wait for process to finish
+/*
+ * wait(int *wstatus) -> pid_t
+ *
+ *  - wait for process to change state
+ *  - Return value:
+ *      - on success, returns the process ID of the terminated child
+ *      - on failure, -1 is returned
+ *  - included in <sys/wait.h>
+ * 
+ * References:
+ *  - https://man7.org/linux/man-pages/man2/wait.2.html
+ */
 
 #include <stdio.h> // flush
 #include <unistd.h> // fork
@@ -6,9 +17,10 @@
 
 int main(int argc, char* argv[])
 {
+    printf("***************************************\n");
     int id = fork();
     int n;
-    if (id == 0) {
+    if (id == 0) { 
         n = 1;
     } else {
         n = 6;
@@ -22,14 +34,13 @@ int main(int argc, char* argv[])
     int i;
     for (i = n; i < n + 5; i++) {
         printf("%d ", i);
-        // With fflush, printf immediatelly prints text to the output, it doesn't beffuers it into
+        // With fflush, printf immediatelly prints text to the output, it doesn't buffers it into
         // internal buffer that it has
-        // - however, I don't see any difference with and wothout fflush in my compiler!
+        // - however, I don't see any difference with and without fflush in my compiler!
         fflush(stdout);
     }
-    if (id != 0) {
-        printf("\n");
-    }
+    printf("\n");
 
+    printf("---------------------------------------\n");
     return 0;
 }
